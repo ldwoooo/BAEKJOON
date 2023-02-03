7275f3d8cc9ba3dd415d5043414647abc7e2d14b
ldwoooo <ldwoo970116@gmail.com>
2023-02-03 17:55:14 +0900

    주석 추가

--- 11단계_재귀/11-3_25501_재귀의귀재.py
+++ 11단계_재귀/11-3_25501_재귀의귀재.py
@@ -1,5 +1,5 @@
 def recursion(s, l, r, c):
-
+    # 함수에 들어오면 c++
     c += 1
 
     if l >= r:  
@@ -12,14 +12,18 @@ def recursion(s, l, r, c):
         return recursion(s, l+1, r-1, c)
 
 def isPalindrome(s):
+    # 재귀함수 호출 횟수를 나타내는 변수 c 선언, 재귀함수의 위치 인자 추가
     c = 0
-    return recursion(s, 0, len(s)-1, c)
+    return recursion(s, 0, len(s)-1, c )
 
 N = int(input())
-words = []
-for i in range(N):
-    word = input()
-    words.append(word)
+
+words = [input() for i in range(N)]
+# 아래와 같은 의미
+# words = []
+# for i in range(N):
+#     word = input()
+#     words.append(word)
 
 for j in words:
     print(*isPalindrome(j))
