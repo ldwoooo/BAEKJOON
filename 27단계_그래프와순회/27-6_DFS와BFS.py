0c7ef023aa38c3d3b284bb7778994b784c97b68b
ldwoooo <ldwoo970116@gmail.com>
2023-03-13 01:07:24 +0900

    .

--- 27단계_그래프와순회/27-6_DFS와BFS.py
+++ 27단계_그래프와순회/27-6_DFS와BFS.py
@@ -1,3 +1,4 @@
+# 틀림. 다시 풀어봐야함
 def dfs(v):
     visited = [0] * (N + 1)                 # 방문행렬
     visited[v] = 1
@@ -12,12 +13,12 @@ def dfs(v):
             if adj_dfs[location][node] == 1 and visited[node] == 0:     # 간선이 있고 방문하지 않았으면
 
                 visited[node] = 1           # 방문체크
-                location = node             # 방문 노드를 시작 위치로
+                location = node             # 방문 노드를 현재 위치로
                 stack.append(node)          # 방문 노드 넣어주기
                 ans.append(node)
                 break
 
-        else:
+        else:                               # 브레이크를 만나지 않고 나오면(갈 길이 없으면) 뒤로 돌아가보자
             if stack:
                 location = stack.pop()
             else:
